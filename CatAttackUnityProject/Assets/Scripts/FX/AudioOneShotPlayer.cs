a7edf3ecfcb6d87e235167d18b7e091093fd242b
HectorColasValtuena <MephistoII666@gmail.com>
2020-06-25 15:48:38 +0200

    added audio to star dash manouver

---
+++ CatAttackUnityProject/Assets/Scripts/FX/AudioOneShotPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOneShotPlayer : MonoBehaviour
+{
+	public AudioSource[] audioSources;
+
+	public void PlayAudio (int index)
+	{
+		audioSources[index].Play();
+	}
+}
