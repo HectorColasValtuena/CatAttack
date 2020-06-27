2e2887831cf4a5620623f3a1ac7023425e9c5f94
HectorColasValtuena <MephistoII666@gmail.com>
2020-06-27 23:24:02 +0200

    implemented starflower prototype

---
+++ CatAttackUnityProject/Assets/Scripts/Environment/PickupGainMaxStarpower.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace CatAttack
+{
+	public class PickupGainMaxStarpower : MonoBehaviour
+	{
+		private const string requiredTag = "Player";
+		private const int starpowerGained = 1;
+		
+		void OnTriggerEnter2D (Collider2D other) 
+		{
+			if (other.tag == requiredTag)
+			{
+				StarpowerReservoir spReservoir = other.GetComponent<StarpowerReservoir>();
+				if (spReservoir == null)
+				{
+					Debug.LogError("PickupGainMaxStarpower found player object without StarpowerReservoir.");
+					return;
+				}
+				spReservoir.GainMaximumStarpower(starpowerGained);
+				gameObject.GetComponent<Animator>().SetBool("Depleted", true);
+				Destroy(this);
+			}
+		}
+	}
+}
