dd09d9dfc85d7d0198ba95d1329ee15dd81202ff
HectorColasValtuena <MephistoII666@gmail.com>
2020-06-28 20:28:38 +0200

    Implemented progression manager and play level buttons

---
+++ CatAttackUnityProject/Assets/Scripts/GameMechanics/ProgressionManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CatAttack
+{
+	public static class ProgressionManager
+	{
+		private const string levelUnlockedKey = "levelUnlocked";
+		private const int defaultUnlockedLevel = 1;
+
+		public static int currentLevel = 0;
+
+		//get if scene is unlocked
+		public static bool LevelIsUnlocked (int levelNumber)
+		{
+			//JSUT IN CSAE guaranteed true on 1 or less
+			if (levelNumber <= defaultUnlockedLevel) return true;
+			
+			//If playerprefs is uninitialized, set up default value
+			if (!PlayerPrefs.HasKey(levelUnlockedKey)) { PlayerPrefs.SetInt(levelUnlockedKey, defaultUnlockedLevel); return levelNumber <= defaultUnlockedLevel; }
+			return levelNumber <= PlayerPrefs.GetInt(levelUnlockedKey);
+		}
+
+		//unlock target scene
+		public static void LevelUnlock (int levelNumber)
+		{
+			//only write playerprefs if not unlocked already to avoid closing later levels
+			if (!ProgressionManager.LevelIsUnlocked(levelNumber)) { PlayerPrefs.SetInt(levelUnlockedKey, levelNumber); }
+		}
+
+		//open target scene
+		public static void LevelChange (int levelNumber, bool autoUnlock = true)
+		{
+			if (autoUnlock) { ProgressionManager.LevelUnlock(levelNumber); }
+			SceneManager.LoadScene(levelNumber);		
+		}
+	}
+}
---
+++ CatAttackUnityProject/Assets/Scripts/UI/MenuPlayLevelButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CatAttack
+{
+	public class MenuPlayLevelButton : MonoBehaviour
+	{
+		public int levelNumber;
+
+		public void Start ()
+		{
+			if (!ProgressionManager.LevelIsUnlocked(levelNumber))
+			{
+				gameObject.SetActive(false);
+			}
+		}
+
+		public void PlayLevelButtonPressed ()
+		{
+			ProgressionManager.LevelChange(levelNumber);
+		}
+	}
+}
