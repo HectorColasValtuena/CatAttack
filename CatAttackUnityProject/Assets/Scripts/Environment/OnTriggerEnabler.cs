48ef2b8db65157a42019dbb44f23f1721b75f268
HectorColasValtuena <MephistoII666@gmail.com>
2020-06-30 01:36:07 +0200

    Implemented letterboard with enabler/disabler on trigger

---
+++ CatAttackUnityProject/Assets/Scripts/Environment/OnTriggerEnabler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace CatAttack
+{		
+	public class OnTriggerEnabler : MonoBehaviour
+	{
+		public GameObject onOffTarget;
+		public bool startDisabled = true;
+
+		public void Awake ()
+		{
+			if (startDisabled) { onOffTarget.SetActive(false); }
+		}
+
+		public void OnTriggerEnter2D (Collider2D other)
+		{
+			onOffTarget.SetActive(true);
+		}
+
+		public void OnTriggerExit2D (Collider2D other)
+		{
+			onOffTarget.SetActive(false);
+		}
+	}
+}
