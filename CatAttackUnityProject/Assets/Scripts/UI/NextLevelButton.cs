1cf79115d94689195bab7ba6bbb65bdd4ddee218
HectorColasValtuena <MephistoII666@gmail.com>
2020-06-29 23:47:14 +0200

    praying mantis

---
+++ CatAttackUnityProject/Assets/Scripts/UI/NextLevelButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CatAttack
+{
+	public class NextLevelButton : MonoBehaviour
+	{
+		public void NextLevelButtonPressed ()
+		{
+			LevelManager.instance.AdvanceLevel();
+		}
+	}
+}
