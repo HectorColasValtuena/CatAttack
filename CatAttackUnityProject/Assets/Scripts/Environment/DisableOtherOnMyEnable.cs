4dab66e17c7e3f83dd3d652a60ab2c9bde38357b
HectorColasValtuena <MephistoII666@gmail.com>
2020-07-07 01:52:39 +0200

    Napstablook says hi

---
+++ CatAttackUnityProject/Assets/Scripts/Environment/DisableOtherOnMyEnable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DisableOtherOnMyEnable : MonoBehaviour
+{
+	public GameObject targetGameObject;
+
+	public void OnEnable () 
+	{
+		targetGameObject.SetActive(false);
+	}
+
+	public void OnDisable ()
+	{
+		targetGameObject.SetActive(true);
+	}
+}
