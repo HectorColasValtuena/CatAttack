ad03c6a8bb3e2a70aaf1236229741cdfddb4417a
HectorColasValtuena <MephistoII666@gmail.com>
2020-07-07 23:31:36 +0200

    quest progress

---
+++ CatAttackUnityProject/Assets/Scripts/QuestDynamite/DynamiteQuestController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class DynamiteQuestController : MonoBehaviour
+{
+	public GameObject[] dialogList;
+
+	private int phase = 0;
+
+	public void Start()
+	{
+		
+	}
+
+	//Advances the phase index and enables target gameobject within the list. if parameter is negative or omitted will automatically advance 1 step
+	public void AdvancePhase (int targetPhase = -1)
+	{
+		if (targetPhase < 0) { targetPhase = phase+1; }
+		if (targetPhase <= phase) { return; }
+		phase = targetPhase;
+		ChangeDialog(targetPhase);
+	}
+
+	private void ChangeDialog (int targetDialog)
+	{
+		for (int i = 0, iLimit = dialogList.Length; i < iLimit; i++)
+		{
+			dialogList[i].SetActive(i == targetDialog);
+		}
+	}
+}
