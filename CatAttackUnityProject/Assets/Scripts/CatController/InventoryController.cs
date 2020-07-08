417d096817f9de7ca30acd7d130b0ce38167ae99
HectorColasValtuena <MephistoII666@gmail.com>
2020-07-08 02:51:43 +0200

    moar quest progress

---
+++ CatAttackUnityProject/Assets/Scripts/CatController/InventoryController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+	//object containing carried dynamite sprite
+	public GameObject dynamiteFX;
+
+	public bool _dynamite = false;
+	public bool hasDynamite 
+	{
+		get { return _dynamite; }
+		set
+		{
+			_dynamite = value;
+			if (dynamiteFX != null)
+			{
+				dynamiteFX.SetActive(value);
+			}
+		}
+	}
+
+}
--- CatAttackUnityProject/Assets/Scripts/QuestDynamite/DynamiteQuestController.cs
+++ CatAttackUnityProject/Assets/Scripts/QuestDynamite/DynamiteQuestController.cs
@@ -1,30 +1,48 @@
 ﻿using UnityEngine;
 
-public class DynamiteQuestController : MonoBehaviour
+namespace CatAttack
 {
-	public GameObject[] dialogList;
-
-	private int phase = 0;
-
-	public void Start()
+	public class DynamiteQuestController : MonoBehaviour
 	{
-		
-	}
+		public GameObject[] dialogList;	//list of dialog containers for respective phases
+		public Animator animator;
 
-	//Advances the phase index and enables target gameobject within the list. if parameter is negative or omitted will automatically advance 1 step
-	public void AdvancePhase (int targetPhase = -1)
-	{
-		if (targetPhase < 0) { targetPhase = phase+1; }
-		if (targetPhase <= phase) { return; }
-		phase = targetPhase;
-		ChangeDialog(targetPhase);
-	}
+		//No, this is dealt with by the animation
+		//public GameObject rubbleBlockage;	//gameobject of the rubble pile blocking the path - disable upon completion
+		//public GameObject explosionFX;		//effects to play upon blowing up the rubble
 
-	private void ChangeDialog (int targetDialog)
-	{
-		for (int i = 0, iLimit = dialogList.Length; i < iLimit; i++)
+		private int phase = 0;
+
+		public void Awake ()
 		{
-			dialogList[i].SetActive(i == targetDialog);
+			phase = 0;
+			if (animator == null) { animator = GetComponent<Animator>(); }
+		}
+
+		//Advances the phase index and enables target gameobject within the list. if parameter is negative or omitted will automatically advance 1 step
+		public void AdvancePhase (int targetPhase = -1)
+		{
+			if (targetPhase < 0) { targetPhase = phase+1; }
+			if (targetPhase <= phase) { return; }
+			phase = targetPhase;
+			ChangeDialog(targetPhase);
+
+			//phase specific checks - optimize this?
+			if (phase == 2)
+			{
+				//Blow-up the wall upon reaching phase 2
+				//play the animation blowing up the wall
+				animator.SetBool("DynamiteBrought", true);
+			}
+			//else if (phase == 3)
+		}
+
+		private void ChangeDialog (int targetDialog)
+		{
+			for (int i = 0, iLimit = dialogList.Length; i < iLimit; i++)
+			{
+				dialogList[i].SetActive(i == targetDialog);
+			}
 		}
 	}
 }
