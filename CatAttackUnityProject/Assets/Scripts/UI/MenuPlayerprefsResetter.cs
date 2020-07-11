bd2347fbdd8f0b70b285aca5e05cd5285eeee01a
HectorColasValtuena <MephistoII666@gmail.com>
2020-07-11 03:01:22 +0200

    menu reset button

---
+++ CatAttackUnityProject/Assets/Scripts/UI/MenuPlayerprefsResetter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace CatAttack
+{
+	public class MenuPlayerprefsResetter : MonoBehaviour
+	{
+		public void ResetPlayerprefsButton()
+		{
+			Debug.LogWarning("ResettingPlayerprefs");
+			PlayerPrefs.DeleteAll();
+			PlayerPrefs.Save();
+			ProgressionManager.LevelChange(0);
+		}
+	}
+}
