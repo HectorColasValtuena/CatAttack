f084b2fc6d90e1313b9d896fc9791317960eea07
HectorColasValtuena <MephistoII666@gmail.com>
2020-07-11 02:24:44 +0200

    created playerprefs resetter for in-editor reset

---
+++ CatAttackUnityProject/Assets/Scripts/Dev/PlayerprefsResetter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+ 
+public class PlayerprefsResetter : MonoBehaviour
+{
+    /// Add a context menu named "Do Something" in the inspector
+    /// of the attached script.
+    [ContextMenu("Reset Playerprefs")]
+    void ResetPlayerprefs()
+    {
+        Debug.LogWarning("ResettingPlayerprefs");
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+}
