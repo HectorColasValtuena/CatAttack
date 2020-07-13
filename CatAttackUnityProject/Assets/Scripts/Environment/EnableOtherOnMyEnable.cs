1fae422e7afd56835cb7ef94b6c66d29c6e64960
HectorColasValtuena <MephistoII666@gmail.com>
2020-07-13 17:42:02 +0200

    musica fiesta

---
+++ CatAttackUnityProject/Assets/Scripts/Environment/EnableOtherOnMyEnable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EnableOtherOnMyEnable : MonoBehaviour
+{
+	public GameObject targetGameObject;
+
+	public bool reDisableOnMyDisable = true;
+
+	public void OnEnable () 
+	{
+		targetGameObject.SetActive(true);
+	}
+
+	public void OnDisable ()
+	{
+		if (reDisableOnMyDisable) { targetGameObject.SetActive(false); }
+	}
+}
