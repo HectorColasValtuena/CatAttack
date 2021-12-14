1d0b1a7b496bf6ff916f81a15a22d3b55416a6f5
HectorColasValtuena <MephistoII666@gmail.com>
2021-12-14 10:38:14 +0100

    tabs

--- CatAttackUnityProject/Assets/Scripts/CatController/Platformer2DUserControl.cs
+++ CatAttackUnityProject/Assets/Scripts/CatController/Platformer2DUserControl.cs
@@ -4,40 +4,40 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace CatAttack
 {
-    [RequireComponent(typeof (PlatformerCharacter2D))]
-    public class Platformer2DUserControl : MonoBehaviour
-    {
-        public static Rigidbody2D playerCatRigidbody;
+	[RequireComponent(typeof (PlatformerCharacter2D))]
+	public class Platformer2DUserControl : MonoBehaviour
+	{
+		public static Rigidbody2D playerCatRigidbody;
 
-        private PlatformerCharacter2D m_Character;
-        private bool m_Jump;
-
-
-        private void Awake()
-        {
-            Platformer2DUserControl.playerCatRigidbody = gameObject.GetComponent<Rigidbody2D>();
-            m_Character = GetComponent<PlatformerCharacter2D>();
-        }
+		private PlatformerCharacter2D m_Character;
+		private bool m_Jump;
 
 
-        private void Update()
-        {
-            if (!m_Jump)
-            {
-                // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            }
-        }
+		private void Awake()
+		{
+			Platformer2DUserControl.playerCatRigidbody = gameObject.GetComponent<Rigidbody2D>();
+			m_Character = GetComponent<PlatformerCharacter2D>();
+		}
 
 
-        private void FixedUpdate()
-        {
-            // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            // Pass all parameters to the character control script.
-            m_Character.Move(h, crouch, m_Jump);
-            m_Jump = false;
-        }
-    }
+		private void Update()
+		{
+			if (!m_Jump)
+			{
+				// Read the jump input in Update so button presses aren't missed.
+				m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+			}
+		}
+
+
+		private void FixedUpdate()
+		{
+			// Read the inputs.
+			bool crouch = Input.GetKey(KeyCode.LeftControl);
+			float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			// Pass all parameters to the character control script.
+			m_Character.Move(h, crouch, m_Jump);
+			m_Jump = false;
+		}
+	}
 }
