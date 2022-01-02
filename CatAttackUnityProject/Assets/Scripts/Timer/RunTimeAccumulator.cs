fc0c6bc9152dd0d48d36a14e9ca2406554824d5d
HectorColasValtuena <MephistoII666@gmail.com>
2022-01-02 01:05:01 +0100

    RunTimeAccumulator prototype

---
+++ CatAttackUnityProject/Assets/Scripts/Timer/RunTimeAccumulator.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+
+using Debug = UnityEngine.Debug;
+
+namespace CatAttack
+{
+	public static class RunTimeAccumulator
+	{
+	//const	
+	//ENDOF const
+
+	//public static members
+		public static bool FullRunTimeAvailable
+		{ get {/*[TO-DO]*/ return false; } }
+
+		public static void RegisterLevelTime (int level, float seconds)
+		{
+			Debug.Log(string.Format("Registered level {0} time: {1}", level, seconds));
+			if (levelTimes.ContainsKey(level))
+			{ levelTimes.Remove(level); }
+
+			levelTimes.Add(level, seconds);
+		}
+
+		//returns level # time, or null if not registered
+		public static float? GetLevelTime (int level, float seconds)
+		{
+			if (levelTimes.ContainsKey(level))
+			{ return levelTimes[level]; }
+			return null;
+		}
+
+		public static void ResetTimeAccumulator ()
+		{
+			ResetTimeDictionary();
+		}
+	//ENDOF public static
+
+	//private static fields
+		//a dictionary matching level # with its stored time
+		private static IDictionary<int, float> levelTimes;
+		private static void ResetTimeDictionary ()
+		{ levelTimes = new Dictionary<int,float>(); }
+	//ENDOF private static
+	}
+}
