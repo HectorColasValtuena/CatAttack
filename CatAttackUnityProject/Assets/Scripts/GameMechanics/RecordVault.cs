c8a11626205759904ddd168ca8760a5421072116
HectorColasValtuena <MephistoII666@gmail.com>
2022-01-13 11:20:40 +0100

    record vault static class (untested)

---
+++ CatAttackUnityProject/Assets/Scripts/GameMechanics/RecordVault.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace CatAttack
+{
+	public static class RecordVault
+	{
+		private const string RECORD_KEY = "RunRecordTime";
+
+	//public properties
+		public static bool HasRecord { get { return PlayerPrefs.HasKey(RECORD_KEY); }}
+
+		public static float Record
+		{
+			get { return PlayerPrefs.GetFloat(RECORD_KEY); }
+			private set { PlayerPrefs.SetFloat(RECORD_KEY, value); }
+		}
+	//ENDOF public properties
+
+	//public methods
+		public static bool IsRecord (float seconds)
+		{
+			return (seconds < Record);
+		}
+
+		public static void RegisterTime (float seconds)
+		{
+			if (IsRecord(seconds))
+			{
+				Record = seconds;
+			}
+		}
+	//ENDOF public methods
+	}
+}
