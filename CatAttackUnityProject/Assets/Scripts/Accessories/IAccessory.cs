d368e3dc8144ffc631afda32e9812ae042b9b2aa
HectorColasValtuena <MephistoII666@gmail.com>
2022-01-24 17:37:00 +0100

    IAccessory added Enabled and Item properties

--- CatAttackUnityProject/Assets/Scripts/Accessories/IAccessory.cs
+++ CatAttackUnityProject/Assets/Scripts/Accessories/IAccessory.cs
@@ -1,7 +1,15 @@
+using EItemID = CatAttack.Inventory.EItemID;
+
 namespace CatAttack.Accessories
 {
 	public interface IAccessory
 	{
+		//gets and sets wether this accessory is active
+		bool Enabled { get; set; }
+
+		//inventory item represented by this accessory
+		EItemID Item { get; }
+
 		//update this accessory's pose to fit parent sprite and horizontal flip
 		void UpdatePose (UnityEngine.Sprite sprite, bool flip);
 	}
