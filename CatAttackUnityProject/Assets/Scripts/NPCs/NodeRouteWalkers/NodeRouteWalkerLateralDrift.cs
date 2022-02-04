697d602a84049c01e5f5a6c72e2675c6052146e4
HectorColasValtuena <MephistoII666@gmail.com>
2022-02-04 16:36:19 +0100

    NodeRouteWalkerLateralDrift variant created

---
+++ CatAttackUnityProject/Assets/Scripts/NPCs/NodeRouteWalkers/NodeRouteWalkerLateralDrift.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+using static CatAttack.Extensions.Transform2DExtensions;	//Transform.ELookAt2D(target)
+
+using FloatRange = CatAttack.CatMath.Ranges.FloatRange;
+
+namespace CatAttack.NodeRouteWalkers
+{
+	//this is the simplest node route walker
+	//moves in a straight line from node to node
+	public class NodeRouteWalkerLateralDrift :
+		NodeRouteWalkerBase
+	{
+	//serialized fields
+		[SerializeField]
+		private FloatRange lateralImpulseRange;
+
+		[SerializeField]
+		private float lateralDecelerationRate;
+	//ENDOF serialized
+
+	//private fields
+		private float lateralVelocity;
+	//ENDOF private fields
+
+	//private properties
+		private float lateralFrameMovement { get { return this.lateralVelocity * Time.deltaTime; }}
+	//ENDOF private properties
+
+	//overrides
+		protected override void MoveTowardsNode ()
+		{
+			base.MoveTowardsNode();
+
+			this.transform.Translate(x: 0, y: this.lateralFrameMovement, z: 0);
+			this.lateralVelocity *= 1 - this.lateralDecelerationRate;
+		}
+
+		protected override void OnNodeReached ()
+		{
+			//base.OnNodeReached();
+			this.lateralVelocity = this.lateralImpulseRange.random;
+		}
+	//ENDOF overrides
+
+	//private methods
+	//ENDOF private methods
+	}
+}
