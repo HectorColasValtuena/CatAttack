e4d8ecfbce81a6c0d98edc77f9d03c155eb2bca0
HectorColasValtuena <MephistoII666@gmail.com>
2022-02-26 04:47:03 +0100

    TouchInputController fixed quick jump direction switches detecting a singe jump

--- CatAttackUnityProject/Assets/Scripts/Input/InputControllers/TouchInputController.cs
+++ CatAttackUnityProject/Assets/Scripts/Input/InputControllers/TouchInputController.cs
@@ -30,8 +30,36 @@ namespace CatAttack.Input
 	//ENDOF IInputController implementation
 
 	//private properties
+		private EJumpState currentJumpState
+		{
+			get
+			{
+				//ensure no overlap occurs:
+				//when quickly switching between jump directions, cause a button lift for one frame to ensure new jump capture
+				if (this.rightJumpButton.pressed && this.lastJumpState == EJumpState.left
+				 || this.leftJumpButton.pressed && this.lastJumpState == EJumpState.right)
+				{ return EJumpState.none; }
+
+				//if only right side or only left side are presed, return that direction
+				if (this.rightJumpButton.pressed && !this.leftJumpButton.pressed)
+				{ return EJumpState.right; }
+
+				if (this.leftJumpButton.pressed && !this.rightJumpButton.pressed)
+				{ return EJumpState.left; }
+
+
+				//if neither direction, return none
+				if (!this.rightJumpButton.pressed && !this.leftJumpButton.pressed)
+				{ return EJumpState.none; }
+
+				//if neither previous possibility happens, maintain current jump state
+				//if (this.rightJumpButton.pressed && this.leftJumpButton.pressed)
+				return this.lastJumpState;
+			}
+		}
+
 		private bool jumpInput
-		{ get { return this.rightJumpButton.pressed || this.leftJumpButton.pressed; }}
+		{ get { return this.currentJumpState == EJumpState.left || this.currentJumpState == EJumpState.right; }}
 
 		private bool leftInput
 		{ get { return this.leftWalkButton.pressed || this.leftJumpButton.pressed; }}
@@ -54,12 +82,25 @@ namespace CatAttack.Input
 				else
 				{ this.horizontalAxis = 1f; }
 			}
+
+			//update jump state token
+			this.lastJumpState = this.currentJumpState;
 		}
 	//ENDOF MonoBehaviour lifecycle
 
 	//private fields
 		private bool jump = false;
 		private float horizontalAxis = 0f;
+
+		private EJumpState lastJumpState = EJumpState.none;
 	//ENDOF private fields
+
+	//private enums
+		private enum EJumpState
+		{
+			none,
+			left,
+			right
+		}
 	}
 }
