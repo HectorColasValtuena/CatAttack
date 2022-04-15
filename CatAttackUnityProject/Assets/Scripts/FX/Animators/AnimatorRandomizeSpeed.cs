78b7b98b72def181651077cf223484de2cd4aacd
HectorColasValtuena <MephistoII666@gmail.com>
2022-04-15 14:37:40 +0200

    created AnimatorRandomizeSpeed.cs component

---
+++ CatAttackUnityProject/Assets/Scripts/FX/Animators/AnimatorRandomizeSpeed.cs
@@ -0,0 +1,43 @@
+using SerializeField = UnityEngine.SerializeField;
+
+using Animator = UnityEngine.Animator;
+using Random = UnityEngine.Random;
+
+using RandomFloatRange = PHATASS.Utils.MathUtils.Ranges.RandomFloatRange;
+using ILimitedRangeFloat = PHATASS.Utils.MathUtils.Ranges.ILimitedRange<float>;
+
+namespace CatAttack.FX
+{
+	//This gives the animator a random delay
+	public class AnimatorRandomizeSpeed : UnityEngine.MonoBehaviour
+	{
+	//serialized fields
+		[SerializeField]
+		private Animator animator;
+
+		[SerializeField]
+		private RandomFloatRange _speedModifierRange = new RandomFloatRange(0.75f, 1.25f);
+		private ILimitedRangeFloat speedModifierRange { get { return this._speedModifierRange; }}
+
+	//ENDOF serialized fields
+
+	//MonoBehaviour lifecycle
+		private void Start ()
+		{
+			if (this.animator == null) { this.animator = this.GetComponent<Animator>(); }
+
+			this.RandomizeAnimatorSpeed(this.animator);
+
+			UnityEngine.Object.Destroy(this);
+		}
+	//ENDOF MonoBehaviour lifecycle
+
+	//private methods
+		//delays this animator
+		private void RandomizeAnimatorSpeed (Animator animator)
+		{
+			animator.speed *= this.speedModifierRange.random;
+		}
+	//ENDOF private methods
+	}
+}
