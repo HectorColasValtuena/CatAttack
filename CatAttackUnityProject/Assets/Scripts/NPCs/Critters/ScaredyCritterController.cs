37274a80c83a565fae577aad4609c6a19cd48d1b
HectorColasValtuena <MephistoII666@gmail.com>
2022-04-21 13:18:40 +0200

    ScaredyCritterController completed

--- CatAttackUnityProject/Assets/Scripts/NPCs/Critters/ScaredyCritterController.cs
+++ CatAttackUnityProject/Assets/Scripts/NPCs/Critters/ScaredyCritterController.cs
@@ -8,9 +8,15 @@
 
 using UnityEngine;
 
+using static PHATASS.Utils.Extensions.IListExtensions;
+
+using ILimitedRangeFloat = PHATASS.Utils.MathUtils.Ranges.ILimitedRange<float>;
+
+using IMovementController = CatAttack.MovementControllers.IMovementController;
+
 namespace CatAttack.Critters
 {
-	public class ScaredyCritterController
+	public class ScaredyCritterController : MonoBehaviour
 	{
 	//serialized fields
 		//list of transforms representing the various resting places the critter will stay when idle
@@ -23,7 +29,80 @@ namespace CatAttack.Critters
 
 		//time to stay scared after no longer detecting the player
 		[SerializeField]
-		private RandomFloatRange scaredTime;
+		private PHATASS.Utils.MathUtils.Ranges.RandomFloatRange _scaredTimeRange;
+		private ILimitedRangeFloat scaredTimeRange { get { return this._scaredTimeRange; }}
 	//ENDOF serialized
+
+	//private fields
+		private IMovementController movementController;
+
+		//remaining time scared
+		private float scaredTimer = 0.0f;
+	//ENDOF private fields
+
+	//MonoBehaviour
+		private void Awake ()
+		{
+			this.movementController = this.GetComponent<IMovementController>();
+		}
+
+		private void Start ()
+		{
+			//ensure a target is chosen at the start
+			this.NewTarget();
+		}
+
+		private void Update ()
+		{
+			if (this.isScared)
+			{
+				//update scared timer if scared
+				this.scaredTimer -= Time.deltaTime;
+
+				//if no longer scared anymore, or arrived at current target, generate new destination
+				if (!this.isScared || this.movementController.arrived)
+				{
+					this.NewTarget();
+				}
+			}
+		}
+
+		private void OnTriggerEnter2D (Collider2D other)
+		{
+			Debug.Log("" + other.gameObject.name + " scared me!");
+			this.Scare();
+		}
+	//ENDOF MonoBehaviour
+
+	//private properties
+		private bool isScared
+		{ get { return this.scaredTimer >= 0; }}
+	//ENDOF private properties
+
+	//private methods
+		private void Scare ()
+		{
+			this.scaredTimer = this.scaredTimeRange.random;
+			this.NewTarget();
+		}
+
+		private void NewTarget ()
+		{
+			Transform[] candidates;
+
+			if (this.isScared)
+			{ candidates = this.safePlaces; }
+			else 
+			{ candidates = this.restingPlaces; }
+
+			this.SetTarget(candidates.ERandomElement<Transform>());
+		}
+
+		private void SetTarget (Transform target)
+		{
+			this.movementController.targetPosition = target.position;
+			this.movementController.targetRotation = target.rotation;
+		}
+	//ENDOF private methods
 	}
 }
