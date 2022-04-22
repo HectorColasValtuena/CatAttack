ceee630373d810dbda60e9047bee3209fa602eed
HectorColasValtuena <MephistoII666@gmail.com>
2022-04-22 08:32:57 +0200

    RigidbodyFlappyFlierPrototype

--- CatAttackUnityProject/Assets/Scripts/NPCs/MovementControllers/RigidbodyFlappyFlier.cs
+++ CatAttackUnityProject/Assets/Scripts/NPCs/MovementControllers/RigidbodyFlappyFlier.cs
@@ -27,7 +27,7 @@ namespace CatAttack.MovementControllers
 		[Tooltip("Maximum angular randomization given to each flap")]
 		[SerializeField]
 		private RandomFloatRange _angularRandomRange;
-		private ILimitedRangeFloat angularRandomRange { get { return this._angularRandomRange; }}
+		private ILimitedRangeFloat angularDeviationRange { get { return this._angularRandomRange; }}
 
 		[Tooltip("Force to apply with each flap")]
 		[SerializeField]
@@ -36,6 +36,10 @@ namespace CatAttack.MovementControllers
 
 		[Tooltip("Flaps made closer than this distance to the target will diminish in force and randomization")]
 		private float closeApproximationDistance = 0.2f;
+
+		[Tooltip("Animator trigger name to set to true on each flap - none if empty")]
+		[SerializeField]
+		private string flapAnimationTriggerName = "Flap";
 	//ENDOF serialized fields
 
 	//IMovementController implementation
@@ -56,22 +60,19 @@ namespace CatAttack.MovementControllers
 		private void Awake ()
 		{
 			this.rigidbody = this.GetComponent<Rigidbody2D>();
+			if (this.flapAnimationTriggerName != "") { this.animator = this.GetComponent<Animator>(); }
 		}
 
 		private void Update ()
 		{
-			
+			this.UpdateFlight();
 		}
 	//ENDOF MonoBehaviour
 
-	//private fields
-		private new Rigidbody2D rigidbody;
-
-		private Vector2? targetPosition = null;
-		private Quaternion? targetRotation = null;
-	//ENDOF private fields
-
 	//private properties
+		//true while this character is supposed to be in flight
+		private bool flying 
+		{ get { return !this.arrived && this.enabled; }}
 		//returns true if this object is already at its destination
 		private bool arrived
 		{ get { return (this.targetPosition == null || this.distanceToDestination <= this.closeApproximationDistance); }}
@@ -79,6 +80,72 @@ namespace CatAttack.MovementControllers
 		//returns distance between this object and target node
 		private float distanceToDestination
 		{ get { return this.transform.position.EDistanceTo2D((Vector2) this.targetPosition); }}
+
+		//returns a multiplier affecting flapping effects based on distance
+		//when straight over the target return 0, at minimum distance or beyond return 1
+		private float flapModifierByDistance
+		{ get {
+			float normalizedProximityMod = //1 when on target, 0 at minimum distance or beyond
+				(new RandomFloatRange(0f, this.closeApproximationDistance) as ILimitedRangeFloat)
+				.ToNormalized(this.distanceToDestination);
+
+			return 1 - normalizedProximityMod;
+		}}
 	//ENDOF private properties
+
+	//private fields
+		private new Rigidbody2D rigidbody = null;
+		private Animator animator = null;
+
+		private Vector2? targetPosition = null;
+		private Quaternion? targetRotation = null;
+
+		private float flapTimer = 0.0f;
+	//ENDOF private fields
+
+	//private methods
+		private void UpdateFlight ()
+		{
+			if (this.flying && this.flapTimer <= 0)
+			{
+				this.Flap();
+			}
+			else
+			{ this.flapTimer -= Time.deltaTime; }
+		}
+
+		private void Flap ()
+		{
+			if (this.animator != null)
+			{ this.animator.SetTrigger(this.flapAnimationTriggerName); }
+
+			this.rigidbody.AddForce(this.GetFlappingForceVector(), ForceMode2D.Impulse);
+			
+			this.flapTimer = this.flapInterval.random;
+		}
+
+		//Creates a vector2 representing the desired forces for current flap
+		private Vector2 GetFlappingForceVector ()
+		{
+			if (this.targetPosition == null)
+			{
+				Debug.Log("RigidbodyFlappyFlier.GetFlappingForceVector() no target position");
+				return Vector2.zero;
+			}
+
+			Vector2 destinationVector = (Vector2) this.targetPosition;
+
+			float modifier = this.flapModifierByDistance;
+
+			//calculate angle with a random deviation
+			float angle = ((Vector2) this.transform.position).EFromToDegrees2D(destinationVector);
+			angle += (this.angularDeviationRange.random * modifier);
+
+			//transform degrees into a force and scale it with modifier and force
+			Vector2 forceVector = angle.EDegreesToVector2() * modifier * this.flapForceRange.random;
+
+			return forceVector;
+		}
+	//ENDOF private methods
 	}
 }
