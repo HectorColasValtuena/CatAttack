13bb2fa82ae52924ca843df1a8d90022609cf6af
HectorColasValtuena <MephistoII666@gmail.com>
2022-04-25 01:56:59 +0200

    IMovementController annotated possible method add

--- CatAttackUnityProject/Assets/Scripts/NPCs/MovementControllers/IMovementController.cs
+++ CatAttackUnityProject/Assets/Scripts/NPCs/MovementControllers/IMovementController.cs
@@ -5,11 +5,14 @@ namespace CatAttack.MovementControllers
 {
 	public interface IMovementController
 	{
-		bool enabled { get; set; }	// Enable/disable this movement controller
+		bool enabled { get; set; }						// Enable/disable this movement controller
 
-		UnityEngine.Vector2? targetPosition { set; }		// Position to move to. NULL = free movement (none/idle)
-		UnityEngine.Quaternion? targetRotation { set; }		// Desired rotation AT THE END of the movement. NULL = free rotation
+		UnityEngine.Vector2? targetPosition { set; }	// Position to move to. NULL = free movement (none/idle)
+		UnityEngine.Quaternion? targetRotation { set; }	// Desired rotation AT THE END of the movement. NULL = free rotation
 
-		bool arrived { get; }	// Returns true if desired position is reached
+		bool arrived { get; }							// Returns true if desired position is reached
+
+	//implement me maybe?
+	//	void Jump (float forceFactor = 1.0f);			// Jump, modifying default jump force/speed by forceFactor
 	}
 }
