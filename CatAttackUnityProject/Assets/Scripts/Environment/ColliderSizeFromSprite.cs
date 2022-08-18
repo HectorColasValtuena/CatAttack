d8282aa91cb8428453c2cf18021f6a68331b58d7
HectorColasValtueña <MephistoII666@gmail.com>
2022-08-18 04:07:33 +0200

    colliderSizeFromSprite changes

--- CatAttackUnityProject/Assets/Scripts/Environment/ColliderSizeFromSprite.cs
+++ CatAttackUnityProject/Assets/Scripts/Environment/ColliderSizeFromSprite.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using static PHATASS.Utils.Extensions.SpriteExtensions;
+
+using UnityEngine;
+
 
 namespace CatAttack
 {
@@ -8,30 +11,81 @@ namespace CatAttack
 
 	public class ColliderSizeFromSprite : MonoBehaviour
 	{
+	//const
 		private static Vector2 sizeToOffsetVector = new Vector2(0f, -0.5f);
+	//ENDOF const
 
-		private BoxCollider2D targetBoxCollider2D;
-		private Rigidbody2D targetRigidbody2D;
-		private SpriteRenderer targetRspriteRenderer;
+	//serialized variables
+		[SerializeField]
+		[Tooltip("if true component will destroy itself after first update")]
+		private bool selfDestruct = true;
 
-		public void Awake ()
+		[Header("Margins")]
+		[Tooltip("Negative values make the collider smaller than the sprite. Positive values make the collider larger than the sprite")]
+		[SerializeField]
+		private float marginTop = 0f;
+		[Tooltip("Negative values make the collider smaller than the sprite. Positive values make the collider larger than the sprite")]
+		[SerializeField]
+		private float marginDown = 0f;
+		[Tooltip("Negative values make the collider smaller than the sprite. Positive values make the collider larger than the sprite")]
+		[SerializeField]
+		private float marginLeft = 0f;
+		[Tooltip("Negative values make the collider smaller than the sprite. Positive values make the collider larger than the sprite")]
+		[SerializeField]
+		private float marginRight = 0f;
+	//ENDOF serialized
+
+	//private variables
+		private BoxCollider2D boxCollider2D;
+		private Rigidbody2D rigidbody2D;
+		private SpriteRenderer spriteRenderer;
+
+		private Sprite lastSprite;
+	//ENDOF private variables
+
+	//MonoBehaviour lifecycle
+		private void Awake ()
 		{
-			ResizeCollider();
+			this.ResizeCollider();
+			this.TrySelfDestruct();
 		}
 
-		public void ResizeCollider ()
+		private void Update ()
 		{
-			targetBoxCollider2D = GetComponent<BoxCollider2D>();
-			targetRigidbody2D = GetComponent<Rigidbody2D>();
-			targetRspriteRenderer = GetComponent<SpriteRenderer>();
+			if (this.spriteRenderer.sprite != this.lastSprite)
+			{ this.ResizeCollider(); }
+		}	
+	//ENDOF MonoBehaviour
+
+	//private properties
+		private Vector2 spritePivot { get { return this.spriteRenderer.sprite.ENormalizedPivot(); }}
+
+		private float verticalMargin { get { return this.marginTop + this.marginDown; }}
+		private float horizontalMargin
+	//ENDOF properties
+
+	//private methods
+		private void ResizeCollider ()
+		{
+			this.boxCollider2D = this.GetComponent<BoxCollider2D>();
+			this.rigidbody2D = this.GetComponent<Rigidbody2D>();
+			this.spriteRenderer = this.GetComponent<SpriteRenderer>();
 
 			//if any required component is unavailable, report a log and exit
-			if (targetBoxCollider2D == null || targetRigidbody2D == null || targetRspriteRenderer == null) { Debug.Log("ColliderSizeFromSprite required components not found!"); return; }
+			if (this.boxCollider2D == null || this.rigidbody2D == null || this.spriteRenderer == null)
+			{ Debug.Log("ColliderSizeFromSprite required components not found!"); return; }
 
-			//Debug.Log(targetRspriteRenderer.size);
+			Sprite sprite 
 
-			targetBoxCollider2D.offset = targetRspriteRenderer.size * sizeToOffsetVector;
-			targetBoxCollider2D.size = targetRspriteRenderer.size;
+			//Debug.Log(spriteRenderer.size);
+
+			this.boxCollider2D.offset = this.spriteRenderer.size * sizeToOffsetVector;
+			this.boxCollider2D.size = this.spriteRenderer.size;
+		}
+
+		private void TrySelfDestruct ()
+		{
+			if (this.selfDestruct) { UnityEngine.Object.Destroy(this); }
 		}
 	}
 }
